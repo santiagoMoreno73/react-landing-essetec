5efea4084eee1ec764d7d9dae7be60288453277e
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-13 22:38:18 -0500

    added herosection

---
+++ src/app/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section id="home" className="hero-section-5">
+      {/* <div className="bg-overlay"></div> */}
+      <div className="container">
+        <div className="justify-content-center row">
+          <div className="col-lg-8">
+            <div className="hero-wrapper text-center mb-4">
+              <p className="font-16 text-uppercase text-white-50">
+                Discover ESSETEC Today
+              </p>
+              <h1 className="hero-title text-white mb-4">
+                Make your Site Amazing & Unique with{" "}
+                <span className="text-primary">ESSETEC</span>
+              </h1>
+              <p className="text-white-50">
+                To an English person, it will seem like simplified English, as a
+                skeptical Cambridge friend of mine occidental
+              </p>
+              <div>
+                <a href="/" className="btn btn-primary mt-2 me-2">
+                  Get started
+                </a>
+
+                <a href="/" className="btn btn-success mt-2 me-2 me-1">
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
