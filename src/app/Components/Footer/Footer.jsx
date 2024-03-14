461c998d6c679590eba1d763e6b8ff00b08af5ce
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-13 22:38:08 -0500

    added footer

---
+++ src/app/Components/Footer/Footer.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4">
+            <div>
+              <h5 className="footer-list-title mb-4">About the Invoza</h5>
+              <p className="">
+                The Invoza is a sed ut perspiciatis unde omnis iste natus error
+                sit voluptatem accusantium doloremque rem eaque
+              </p>
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-2 offset-lg-1">
+            <div>
+              {/* <h5 className="footer-list-title mb-4">Company</h5>
+              <ul className="list-unstyled footer-list-menu">
+                <li>About US</li>
+                <li>Media </li>
+                <li>Career</li>
+                <li>Blog</li>
+              </ul> */}
+            </div>
+          </div>
+          <div className="col-sm-6 col-lg-2"></div>
+          <div className="col-sm-6 col-lg-3">
+            <h5 className="footer-list-title mb-4">Company</h5>
+            <div>
+              <div className="media">
+                R{" "}
+                <div className="media-body">
+                  <p>476 University Drive Ridge, IL 61257</p>
+                </div>
+              </div>
+              <div className="media">
+                R{" "}
+                <div className="media-body">
+                  <p>476 University Drive Ridge, IL 61257</p>
+                </div>
+              </div>
+              <div className="media">
+                R{" "}
+                <div className="media-body">
+                  <p>476 University Drive Ridge, IL 61257</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row mt-4">
+          <div className="text-center">
+            <p className="text-white mb-0">2024 © ESSETEC. Created by ❤️</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
