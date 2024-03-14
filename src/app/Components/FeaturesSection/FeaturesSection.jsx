6314481379625a650d5bc0ac5f972366ca61c317
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-13 22:38:03 -0500

    added features section

---
+++ src/app/Components/FeaturesSection/FeaturesSection.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+const FeaturesSection = () => {
+  return (
+    <section className="section" id="features">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="text-center mb-5">
+              <h5 className="text-primary text-uppercase small-title">
+                Features
+              </h5>
+              <h4 className="mb-4">Key features of the product</h4>
+              <p>
+                It will be as simple as occidental in fact, it will be
+                Occidental.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-5">
+            <div>
+              <div className="avatar-md mb-4">
+                <span className="avatar-title rounded-circle bg-soft-primary">
+                  R
+                </span>
+              </div>
+              <h5>Improve your Marketing business</h5>
+              <p className="mb-4">
+                If several languages coalesce, the grammar of the resulting
+                language is more simple and regular.
+              </p>
+              <div className="row">
+                <div className="col">
+                  <p>Donec quam felis</p>
+                </div>
+                <div className="col">
+                  <p>Donec quam felis</p>
+                </div>
+              </div>
+              <div className="mt-4">
+                <a href="/" className="btn btn-primary">
+                  Learn more R
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="ms-lg-auto col-sm-8 col-lg-5">
+            <div className="border border-light shadow-none mt-5 mt-lg-0 card">
+              <div className="border-0 bg-transparent card-header">
+                <div>
+                  <i className=" text-danger mr-1">r</i>
+                  <i className=" text-warning mr-1 ms-1">r</i>
+                  <i className=" text-success mr-1 ms-1">r</i>
+                </div>
+              </div>
+              <div className="bg-light card-body">
+                <div className="box-shadow">R</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-5 pt-5">
+          <div className="col-sm-8 col-lg-5">
+            <div className="border border-light shadow-none mt-5 mt-lg-0 card">
+              <div className="border-0 bg-transparent card-header">
+                <div>
+                  <i className=" text-danger mr-1">r</i>
+                  <i className=" text-warning mr-1 ms-1">r</i>
+                  <i className=" text-success mr-1 ms-1">r</i>
+                </div>
+              </div>
+              <div className="bg-light card-body">
+                <div className="box-shadow">R</div>
+              </div>
+            </div>
+          </div>
+          <div className="ms-lg-auto col-lg-5">
+            <div>
+              <div className="avatar-md mb-4">
+                <span className="avatar-title rounded-circle bg-soft-primary">
+                  R
+                </span>
+              </div>
+              <h5>Improve your Marketing business</h5>
+              <p className="mb-4">
+                If several languages coalesce, the grammar of the resulting
+                language is more simple and regular.
+              </p>
+              <div className="row">
+                <div className="col">
+                  <p>Donec quam felis</p>
+                </div>
+                <div className="col">
+                  <p>Donec quam felis</p>
+                </div>
+              </div>
+              <div className="mt-4">
+                <a href="/" className="btn btn-primary">
+                  Learn more R
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturesSection;
