11e07966b088d7af244603bab63bcc7912d20bcf
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-14 22:30:00 -0500

    added new sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import Copyright from "./Components/Copyright/Copyright";
 import FeaturesSection from "./Components/FeaturesSection/FeaturesSection";
 import Footer from "./Components/Footer/Footer";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import Navbar from "./Components/Navbar/Navbar";
 import ServiceSection from "./Components/ServicesSection/ServiceSection";
+import ClientsSection from "./Components/ClientsSection/ClientsSection";
+import SolutionSection from "./Components/SolutionsSection/SolutionSection";
+import SubscribeSection from "./Components/SubscribeSection/SubscribeSection";
 
 export default function Home() {
   return (
@@ -11,7 +15,11 @@ export default function Home() {
       <HeroSection />
       <ServiceSection />
       <FeaturesSection />
+      <ClientsSection />
+      <SolutionSection />
+      <SubscribeSection />
       <Footer />
+      <Copyright />
     </div>
   );
 }
