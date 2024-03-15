0839e3946502f77eef482c47d0571407364e4e28
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-14 23:00:19 -0500

    added label a

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="row">
           <div className="col-sm-6 col-lg-4">
             <div>
-              <h5 className="footer-list-title mb-4">About the Invoza</h5>
+              <h5 className="mb-4 footer-list-title">About the Invoza</h5>
               <p className="">
                 The Invoza is a sed ut perspiciatis unde omnis iste natus error
                 sit voluptatem accusantium doloremque rem eaque
@@ -15,22 +15,42 @@ const Footer = () => {
             </div>
           </div>
           <div className="col-sm-6 col-lg-2 offset-lg-1">
-            <h5 className="footer-list-title mb-4">Company</h5>
-            <ul className="list-unstyled footer-list-menu">
-              <li>About Us</li>
-              <li>Media</li>
-              <li>Career</li>
-            </ul>
+            <div>
+              <h5 className="mb-4 footer-list-title">Company</h5>
+              <ul className="list-unstyled footer-list-menu">
+                <li>
+                  <a href="/">About Us</a>
+                </li>
+                <li>
+                  <a href="/">Media</a>
+                </li>
+                <li>
+                  <a href="/">Career</a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className="col-sm-6 col-lg-2">
-            <h5 className="footer-list-title mb-4">More Info</h5>
-            <ul className="list-unstyled footer-list-menu">
-              <li>Pricing</li>
-              <li>For marketing</li>
-              <li>For Ceos</li>
-              <li>For Agencies</li>
-              <li>Our Apps</li>
-            </ul>
+            <div>
+              <h5 className="footer-list-title mb-4">More Info</h5>
+              <ul className="list-unstyled footer-list-menu">
+                <li>
+                  <a href="/">Pricing</a>
+                </li>
+                <li>
+                  <a href="/">For marketing</a>
+                </li>
+                <li>
+                  <a href="/"> For Ceos</a>
+                </li>
+                <li>
+                  <a href="/">For Agencies</a>
+                </li>
+                <li>
+                  <a href="/">Our Apps</a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className="col-sm-6 col-lg-3">
             <h5 className="footer-list-title mb-4">Company</h5>
