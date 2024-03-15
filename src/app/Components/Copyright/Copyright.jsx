3213bda6acfbdebd1468407ec1b567c2f60624f5
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-14 20:59:59 -0500

    added copyright section

---
+++ src/app/Components/Copyright/Copyright.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <section class="bg-primary py-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="float-sm-start">logo</div>
+            <div className="float-sm-end mt-4 mt-sm-0">
+              <p className="copyright-desc text-white mb-0">
+                2024 © Invoza. Created by Themesdesign
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Copyright;
