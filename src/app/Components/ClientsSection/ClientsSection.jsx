dfc38d38cee2a9bb22790106fd395bfe446c0f73
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-14 22:30:51 -0500

    added clientssection

---
+++ src/app/Components/ClientsSection/ClientsSection.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const ClientsSection = () => {
+  return (
+    <section className="section bg-light" id="clients">
+      <div className="container">
+        <div className="justify-content-center row">
+          <div className="col-lg-8">
+            <div className="text-center mb-5">
+              <h5 className="text-primary text-uppercase small-title">
+                Testimonial
+              </h5>
+              <h4 className="mb-3">What our Client Say</h4>
+              <p>
+                It will be as simple as occidental in fact, it will be
+                Occidental.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <h5 className="mb-4">R 3,500 + Satisfied Client</h5>
+            {/* carrousel */}
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col-sm-6 col-xl-3">
+            <div className="client-image">test</div>{" "}
+          </div>
+          <div className="col-sm-6 col-xl-3">
+            <div className="client-image">test</div>{" "}
+          </div>
+          <div className="col-sm-6 col-xl-3">
+            <div className="client-image">test</div>{" "}
+          </div>
+          <div className="col-sm-6 col-xl-3">
+            <div className="client-image">test</div>{" "}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ClientsSection;
