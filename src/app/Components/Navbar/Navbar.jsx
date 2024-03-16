e8eab155e68526da88978899c1a3b18edce89956
santiagoMoreno73 <santimor2009@hotmail.com>
2024-03-15 22:15:31 -0500

    added change navbar styles

--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
+"use client";
 // next
 import Image from "next/image";
 import Link from "next/link";
 // components
 import NavLink from "./NavLink/NavLink";
+// hooks
+import { useState } from "react";
 
 const navLinks = [
   { title: "Home", path: "#about" },
@@ -13,9 +16,24 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  // change color when scrolling
+  const [color, setColor] = useState(false);
+
+  const changeColor = () => {
+    window.scrollY >= 90 ? setColor(true) : setColor(false);
+  };
+
+  window.addEventListener("scroll", changeColor);
+
   return (
     <div className="container">
-      <nav className="navbar navbar-custom sticky sticky-dark navbar-light navbar navbar-expand-lg fixed-top">
+      <nav
+        className={
+          color
+            ? "navbar navbar-custom sticky sticky-dark navbar-light nav-sticky navbar navbar-expand-lg fixed-top"
+            : "navbar navbar-custom sticky sticky-dark navbar-light navbar navbar-expand-lg fixed-top"
+        }
+      >
         <div className="container">
           <Link href="/" className="logo navbar-brand">
             <Image
