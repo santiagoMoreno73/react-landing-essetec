2289e7a4494bfee5e4d0103dc00115d057ad1e43
santiagoMoreno73 <santimor2009@hotmail.com>
2024-04-30 18:56:05 -0500

    added about page components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,31 @@
+"use client";
 import type { NextPage } from "next";
+import { useEffect } from "react";
 
 import { BaseLayout } from "../components/layouts";
+import {
+  MisionSection,
+  HeroSection,
+  VisionSection,
+  OurValues,
+} from "../components/about";
+
+// import AOS
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const AboutPage: NextPage = () => {
+  // useEffect to set animation duration
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <BaseLayout>
-      <h1>Hola a servicios</h1>
+      <HeroSection></HeroSection>
+      <OurValues></OurValues>
+      <MisionSection></MisionSection>
+      <VisionSection></VisionSection>
     </BaseLayout>
   );
 };
