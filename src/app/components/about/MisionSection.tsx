5c50666f39e9897a0b68683ee27e4232fb3f1172
santiagoMoreno73 <santimor2009@hotmail.com>
2024-05-07 20:39:26 -0500

    refactor aboout page

--- src/app/components/about/MisionSection.tsx
+++ src/app/components/about/MisionSection.tsx
@@ -2,47 +2,55 @@ import Image from "next/image";
 
 export const MisionSection = () => {
   return (
-    <section className="section" id="mision">
-      <div className="container">
-        <div className="justify-content-center row">
-          <div className="col-lg-6">
-            <div className="text-center mb-5">
-              <Image
-                className="img-radius img-fluid"
-                src="/images/mision.jpg"
-                alt="mision image"
-                width={0}
-                height={0}
-                layout="responsive"
-                objectFit="contain"
-              />
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="mb-5">
-              {/* <h5 className="text-primary text-uppercase small-title">
-                Acerca de nosotros
-              </h5> */}
-              <h4 className="mb-3">Misión</h4>
-              <p>
-                Proporcionar soluciones integrales que garanticen la protección
-                de propiedades y activos mediante el uso de tecnologías
-                innovadoras.
-                <br />
-                <br />
-                Nos especializamos en soluciones de seguridad electrónica y
-                tecnológica de primera clase. Además, nos enfocamos en asesorar
-                y guiar a empresas y particulares en su camino hacia la
-                transformación digital, ofreciendo servicios de consultoría
-                tecnológica, diseño de infraestructura tecnológica y desarrollo
-                web. Estamos comprometidos con la excelencia, la constante
-                innovación y la mejora continua, para ayudar a nuestros clientes
-                a prosperar en un mundo cada vez más digital.
-              </p>
-            </div>
-          </div>
+    <div className="justify-content-center row mb-5" id="mision">
+      <div className="text-center mb-5">
+        <h5 className="text-primary text-uppercase small-title">
+          Acerca de nosotros
+        </h5>
+        <h4 className="mb-3">Conocé quienes somos</h4>
+        <p>
+          Descubre una amplia gama de servicios diseñados para satisfacer tus
+          necesidades. Desde desarrollo web hasta consultoría en tecnología,
+          nuestro equipo está aquí para brindarte soluciones eficientes y
+          personalizadas.
+        </p>
+      </div>
+
+      <div className="col-lg-6">
+        <div className="text-center mb-5">
+          <Image
+            className="img-radius img-fluid"
+            src="/images/mision.jpg"
+            alt="mision image"
+            width={0}
+            height={0}
+            layout="responsive"
+            objectFit="contain"
+          />
         </div>
       </div>
-    </section>
+      <div className="col-lg-6">
+        <div className="mb-5">
+          {/* <h5 className="text-primary text-uppercase small-title">
+                Acerca de nosotros
+              </h5> */}
+          <h4 className="mb-3">Misión</h4>
+          <p>
+            Proporcionar soluciones integrales que garanticen la protección de
+            propiedades y activos mediante el uso de tecnologías innovadoras.
+            <br />
+            <br />
+            Nos especializamos en soluciones de seguridad electrónica y
+            tecnológica de primera clase. Además, nos enfocamos en asesorar y
+            guiar a empresas y particulares en su camino hacia la transformación
+            digital, ofreciendo servicios de consultoría tecnológica, diseño de
+            infraestructura tecnológica y desarrollo web. Estamos comprometidos
+            con la excelencia, la constante innovación y la mejora continua,
+            para ayudar a nuestros clientes a prosperar en un mundo cada vez más
+            digital.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
--- src/app/components/about/VisionSection.tsx
+++ src/app/components/about/VisionSection.tsx
@@ -1,45 +1,41 @@
 import Image from "next/image";
 export const VisionSection = () => {
   return (
-    <section className="section " id="vision">
-      <div className="container">
-        <div className="justify-content-center row">
-          <div className="col-lg-6">
-            <div className="mb-5">
-              {/* <h5 className="text-primary text-uppercase small-title">
+    <div className="justify-content-center row" id="vision">
+      <div className="col-lg-6">
+        <div className="mb-5">
+          {/* <h5 className="text-primary text-uppercase small-title">
                 Acerca de nosotros
               </h5> */}
-              <h4 className="mb-3">Visión</h4>
-              <p>
-                Ser reconocidos como líderes en soluciones integrales de
-                seguridad electrónica y tecnológica, brindando tranquilidad y
-                confianza a nuestros clientes a través de la implementación de
-                las tecnologías más innovadoras.
-                <br />
-                <br />
-                Nos comprometemos a asegurar entornos seguros y conectados,
-                siendo el aliado estratégico en la transformación digital tanto
-                para empresas como particulares. Nos impulsa la excelencia y la
-                innovación constante, permitiendo a nuestros clientes prosperar
-                y destacarse en un mundo cada vez más exigente.
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="text-center mb-5">
-              <Image
-                className="img-radius img-fluid"
-                src="/images/vision.jpg"
-                alt="vision image"
-                width={0}
-                height={0}
-                layout="responsive"
-                objectFit="contain"
-              />
-            </div>
-          </div>
+          <h4 className="mb-3">Visión</h4>
+          <p>
+            Ser reconocidos como líderes en soluciones integrales de seguridad
+            electrónica y tecnológica, brindando tranquilidad y confianza a
+            nuestros clientes a través de la implementación de las tecnologías
+            más innovadoras.
+            <br />
+            <br />
+            Nos comprometemos a asegurar entornos seguros y conectados, siendo
+            el aliado estratégico en la transformación digital tanto para
+            empresas como particulares. Nos impulsa la excelencia y la
+            innovación constante, permitiendo a nuestros clientes prosperar y
+            destacarse en un mundo cada vez más exigente.
+          </p>
         </div>
       </div>
-    </section>
+      <div className="col-lg-6">
+        <div className="text-center mb-5">
+          <Image
+            className="img-radius img-fluid"
+            src="/images/vision.jpg"
+            alt="vision image"
+            width={0}
+            height={0}
+            layout="responsive"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
