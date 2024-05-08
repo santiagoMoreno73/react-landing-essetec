f6c16ccfd19d503147cdf6e0551f9ea7f4cc6627
santiagoMoreno73 <santimor2009@hotmail.com>
2024-05-07 20:41:08 -0500

    added hero section services

---
+++ src/app/components/service/HeroSection.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export const HeroSection = () => {
+  return (
+    <section id="#home">
+      <div className="hero-container-services">
+        <div className="justify-content-center row">
+          <div className="col-lg-8">
+            <div className="hero-content-about"></div>
+            <div className="hero-wrapper-about text-center mb-4">
+              <Image
+                src="/images/logo_bl.png"
+                width={360}
+                height={360}
+                alt="icon"
+              />
+              <h1 className="hero-title text-white mb-4">
+                Convierte tu negocio en una experiencia tecnológica
+                sorprendente, única e innovadora.{" "}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
