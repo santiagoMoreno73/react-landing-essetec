780c9ed9a27486ddde8726abc80e533e41b07e30
santiagoMoreno73 <santimor2009@hotmail.com>
2024-05-09 23:06:11 -0500

    added herosection component

---
+++ src/app/components/ui/HeroSection.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+
+type HeroSectionProps = {
+  title?: string;
+  description?: string;
+  isVideo?: boolean;
+  imgUrl?: string;
+};
+
+export const HeroSection = ({
+  title,
+  description,
+  imgUrl,
+  isVideo = true,
+}: HeroSectionProps) => {
+  return (
+    <section id="#home">
+      <div className="hero-container">
+        {isVideo ? (
+          <div className="hero-video">
+            <video autoPlay muted loop>
+              <source src="/video/video_essetec.mp4"></source>
+            </video>
+          </div>
+        ) : (
+          <div
+            style={{
+              background: `url(${imgUrl})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "80vh",
+            }}
+          ></div>
+        )}
+        <div className="justify-content-center row">
+          <div className="col-lg-8">
+            <div className="hero-content"></div>
+            <div className="hero-wrapper text-center mb-4">
+              <Image
+                className="img-fluid"
+                src="/images/logos/logo_bl2.png"
+                width={330}
+                height={380}
+                alt="icon"
+              />
+              <h1 className="hero-title text-white mb-4">
+                <span className="font-24">
+                  <b>
+                    <TypeAnimation
+                      sequence={[
+                        "ESPECIALISTAS EN SEGURIDAD ELECTRONICA & TECNOLOGIA",
+                        1000,
+                      ]}
+                      wrapper="span"
+                      speed={50}
+                      repeat={Infinity}
+                    />
+                  </b>
+                </span>
+              </h1>
+              <p className="font-20">
+                Convierte tu negocio en una experiencia <br />
+                tecnológica sorprendente, única e innovadora.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -2,3 +2,4 @@ export { Navbar } from './Navbar';
 export { Footer } from './Footer';
 export { Copyright } from './Copyright';
 export { Counter } from './Counter/Counter';
+export { HeroSection } from './HeroSection';
