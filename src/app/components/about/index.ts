0e025d0945c2326fc89e3175453dde2a2f4da5bc
santiagoMoreno73 <santimor2009@hotmail.com>
2024-05-09 23:05:43 -0500

    remove unused herosection

--- src/app/components/about/index.ts
+++ src/app/components/about/index.ts
@@ -1,5 +1,4 @@
 export { MisionSection } from "./MisionSection";
 export { VisionSection } from "./VisionSection";
 export { OurValues } from "./OurValues";
-export { HeroSection } from "./HeroSection";
 
