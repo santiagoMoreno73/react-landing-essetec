7e787e82237d6a2b562271aa0caeabc24078aeda
santiagoMoreno73 <santimor2009@hotmail.com>
2024-05-10 18:01:28 -0500

    added changes in index file

--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -1,7 +1,7 @@
-export { Navbar } from './Navbar';
-export { Footer } from './Footer';
+export { Navbar } from './Navbar/Navbar';
+export { Footer } from './Footer/Footer';
 export { Copyright } from './Copyright';
-export { HeroSection } from './HeroSection';
+export { HeroSection } from './HeroSection/HeroSection';
 export { Carrousel } from './Carrousel/Carrousel';
 export { Counter } from './Counter/Counter';
 export { FloatButton } from './FloatButton/FloatButton';
