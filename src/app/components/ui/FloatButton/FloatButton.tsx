a55bd211b789f885ceefd14c945af1f2594c703c
santiagoMoreno73 <santimor2009@hotmail.com>
2024-05-20 21:15:32 -0500

    hover transition

--- src/app/components/ui/FloatButton/FloatButton.tsx
+++ src/app/components/ui/FloatButton/FloatButton.tsx
@@ -6,11 +6,14 @@ import { motion } from "framer-motion";
 export const FloatButton = () => {
   return (
     <motion.a
-      whileTap={{ scale: 0.85 }}
       className="btn-float box-shadow"
       type="button"
       href="https://api.whatsapp.com/send?phone=573202063747"
       target="_blank"
+      aria-label="float button"
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.85 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
     >
       <FaWhatsapp className="icon" />
     </motion.a>
