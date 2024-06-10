c5958331803172acf6b640cc1a4373b40285310c
santiagoMoreno73 <santimor2009@hotmail.com>
2024-06-09 20:01:54 -0500

    added monitoring/helpdesk

--- src/app/components/service/ServiceInfraestructure.tsx
+++ src/app/components/service/ServiceInfraestructure.tsx
@@ -21,6 +21,24 @@ const BRANDS_VR = [
     src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/virtualization%2Fvmware.png?alt=media&token=b946a057-bac1-4ea8-a4a5-4044f5aaed32",
     alt: "Hilook-logo",
   },
+  {
+    id: 3,
+    name: "carrousel-img-4",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/virtualization%2FQemu.png?alt=media&token=7ad98179-59ad-4330-9db3-956277aeabc9",
+    alt: "Kemu-logo",
+  },
+  {
+    id: 4,
+    name: "carrousel-img-5",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/virtualization%2FKVM.png?alt=media&token=f258908b-a2b0-4cd9-a06b-6ede3ba6b0e2",
+    alt: "Kvm-logo",
+  },
+  {
+    id: 5,
+    name: "carrousel-img-6",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/virtualization%2Fvirtualbox.png?alt=media&token=ec6704bf-b2e3-4e4b-98a0-b09d191ef45f",
+    alt: "VirtualBox-logo",
+  },
 ];
 
 const BRANDS_CP = [
@@ -47,6 +65,51 @@ const BRANDS_CP = [
     name: "carrousel-img-4",
     src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/cloud-computing%2Famazon_web.png?alt=media&token=a65b3ef6-0712-4939-a99f-502f8cdb3b9e",
     alt: "AWS-logo",
+  },
+  {
+    id: 4,
+    name: "carrousel-img-5",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/cloud-computing%2FOracleCloud.png?alt=media&token=fcd3b428-eca7-4782-a7e6-5c358ca8a73e",
+    alt: "Oracle-logo",
+  },
+];
+
+const BRANDS_MH = [
+  {
+    id: 0,
+    name: "carrousel-img-1",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/help-desk%2FWazuh.png?alt=media&token=8fbc1b84-d3ff-4d1f-853d-0b4e032d4597",
+    alt: "Wazu-logo",
+  },
+  {
+    id: 1,
+    name: "carrousel-img-2",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/help-desk%2Fglpi.png?alt=media&token=6091ea81-e813-466b-bcff-c1bd234ae344",
+    alt: "GLPI-logo",
+  },
+  {
+    id: 2,
+    name: "carrousel-img-3",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/help-desk%2Fnagiios.png?alt=media&token=0f6115c3-b694-4624-a5e4-155de0d4f8cf",
+    alt: "Nagios-logo",
+  },
+  {
+    id: 3,
+    name: "carrousel-img-4",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/help-desk%2FosTicket.png?alt=media&token=e0424df3-2570-4b9d-9ebb-d3c69425fd38",
+    alt: "OSTicket-logo",
+  },
+  {
+    id: 4,
+    name: "carrousel-img-5",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/help-desk%2Fprtg.png?alt=media&token=d0254557-946a-4efb-a1b1-e7629e286c4c",
+    alt: "PRTG-logo",
+  },
+  {
+    id: 5,
+    name: "carrousel-img-6",
+    src: "https://firebasestorage.googleapis.com/v0/b/essetec-base.appspot.com/o/help-desk%2Fzabbix.jpeg?alt=media&token=111ea76d-8b16-4e51-b9d9-1b6afe85af82",
+    alt: "Zabbix-logo",
   },
 ];
 
@@ -165,7 +228,7 @@ export const ServiceInfraestructure = () => {
           </p>
           <div className="row my-4">
             <div className="col">
-              {/* <CarrouselBrand slides={BRANDS} /> */}
+              <CarrouselBrand slides={BRANDS_MH} />
             </div>
           </div>
         </div>
