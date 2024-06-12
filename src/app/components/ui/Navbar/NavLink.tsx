0d4a2f622002758c10612f97a8c9dd9e65609435
santiagoMoreno73 <santimor2009@hotmail.com>
2024-06-11 23:00:42 -0500

    remove unused function

--- src/app/components/ui/Navbar/NavLink.tsx
+++ src/app/components/ui/Navbar/NavLink.tsx
@@ -1,24 +1,8 @@
 import Link from "next/link";
 
 export const NavLink = ({ href, title }) => {
-  const handleClick = (event: any) => {
-    // console.log("EVENT", event);
-    if (href.startsWith("#")) {
-      event.preventDefault();
-
-      const target = document.querySelector(href);
-      // console.log("TARGET", target);
-      if (target) {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
-      }
-    }
-  };
-
   return (
-    <Link href={href} className="nav-link" onClick={handleClick}>
+    <Link href={href} className="nav-link">
       {title}
     </Link>
   );
