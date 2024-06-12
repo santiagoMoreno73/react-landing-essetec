f96b888a4ca794135ff8e263813361dbd5785a74
santiagoMoreno73 <santimor2009@hotmail.com>
2024-06-11 23:00:34 -0500

    added ServiceWeb to index

--- src/app/components/service/index.ts
+++ src/app/components/service/index.ts
@@ -2,3 +2,5 @@ export { CardServiceDetail } from "./CardServiceDetail";
 export { ServiceDetail } from "./ServiceDetail";
 export { ServiceInfraestructure } from "./ServiceInfraestructure";
 export { ServiceSecurity } from "./ServiceSecurity";
+export { ServiceWeb } from "./ServiceWeb";
+
