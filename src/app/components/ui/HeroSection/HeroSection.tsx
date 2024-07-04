f7d18d79d1b3c702d6ee56eb73194bb1ec4d7897
santiagoMoreno73 <santimor2009@hotmail.com>
2024-07-04 09:48:14 -0500

    added motion animation

--- src/app/components/ui/HeroSection/HeroSection.tsx
+++ src/app/components/ui/HeroSection/HeroSection.tsx
@@ -2,6 +2,7 @@
 import { FC } from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 import "./HeroSection.css";
 
 type HeroSectionProps = {
@@ -28,44 +29,49 @@ export const HeroSection: FC<HeroSectionProps> = ({
               autoPlay
               muted
               loop
-            ></video>
+            />
           </div>
         ) : (
           <div
             className="hero-image"
-            style={{
-              background: `url(${imgUrl})`,
-            }}
-          ></div>
+            style={{ backgroundImage: `url(${imgUrl})` }}
+          />
         )}
-        <div className="justify-content-center row">
+        <div className="row justify-content-center">
           <div className="col-lg-8">
-            <div className="hero-content"></div>
-            <div className="hero-wrapper text-center mb-4">
-              <Image
-                className="hero-logo"
-                src="/images/logos/logo_bl2.png"
-                width={330}
-                height={380}
-                alt="icon"
-                priority
-              />
-              <h1 className="hero-title text-white mb-4">
-                <span className="font-24">
-                  <b>
-                    <TypeAnimation
-                      sequence={[
-                        "ESPECIALISTAS EN SEGURIDAD ELECTRÓNICA & TECNOLOGÍA",
-                        1000,
-                      ]}
-                      wrapper="span"
-                      speed={50}
-                      repeat={Infinity}
-                    />
-                  </b>
-                </span>
-              </h1>
-              <p className="hero-text font-22 text-white">{description}</p>
+            <div className="hero-content">
+              <div className="hero-wrapper text-center mb-4">
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <Image
+                    className="hero-logo"
+                    src="/images/logos/logo_bl2.png"
+                    width={330}
+                    height={380}
+                    alt="icon"
+                    priority
+                  />
+                  <h1 className="hero-title text-white mb-4">
+                    <span className="font-24">
+                      <b>
+                        <TypeAnimation
+                          sequence={[
+                            "ESPECIALISTAS EN SEGURIDAD ELECTRÓNICA & TECNOLOGÍA",
+                            1000,
+                          ]}
+                          wrapper="span"
+                          speed={50}
+                          repeat={Infinity}
+                        />
+                      </b>
+                    </span>
+                  </h1>
+                  <p className="hero-text font-22 text-white">{description}</p>
+                </motion.div>
+              </div>
             </div>
           </div>
         </div>
